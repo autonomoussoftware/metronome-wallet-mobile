3759f5e277b24bda3098bb6eaa9097df60261ec3
Gabriel Montes <gabriel@utoctadel.com.ar>
2017-12-08 14:39:02 -0300

    Update module patches and add ESLint config

---
+++ App/lib/eslint-config-bloq/index.js
@@ -0,0 +1,21 @@
+module.exports = {}
+
+// const oldRules = {
+//   rules: {
+//     'arrow-parens': ['warn', 'as-needed'],
+//     'comma-dangle': 'warn',
+//     'comma-spacing': 'warn',
+//     'no-console': 'warn',
+//     'no-extra-parens': 'warn',
+//     'no-use-before-define': 'warn',
+//     'no-var': 'warn',
+//     'object-curly-spacing': ['warn', 'always'],
+//     'object-shorthand': 'warn',
+//     'prefer-const': 'warn',
+//     'prefer-template': 'warn',
+//     'quotes': ['warn', 'single'],
+//     'semi': ['warn', 'never'],
+//     'space-before-function-paren': ['warn', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
+//     'space-infix-ops': 'warn'
+//   }
+// }
