2684ef8533784ad133245b2a2d9390ed9539842b
Gabriel Montes <gabriel@bloq.com>
2018-08-09 00:08:12 -0300

    Fix config and dotenv files

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
+import Contracts from 'metronome-contracts'
+
+import {
+  REACT_APP_CHAIN,
+  REACT_APP_MET_EXPLORER_URL,
+  REACT_APP_SENTRY_DSN
+} from 'react-native-dotenv'
+
 export default {
-  MTN_TOKEN_ADDR:
-    process.env.REACT_APP_MTN_TOKEN_ADDR ||
-    '0x825a2ce3547e77397b7eac4eb464e2edcfaae514',
-  CONVERTER_ADDR:
-    process.env.REACT_APP_CONVERTER_ADDR ||
-    '0x25d99454d94d9459f0abb06009840a48bd04ca44',
-  MTN_EXPLORER_URL:
-    process.env.REACT_APP_MTN_EXPLORER_URL ||
-    'http://explorer.mtn.bloqrock.net',
-  SENTRY_DSN: process.env.REACT_APP_SENTRY_DSN,
+  MTN_TOKEN_ADDR: Contracts.addresses[REACT_APP_CHAIN].metToken,
+  CONVERTER_ADDR: Contracts.addresses[REACT_APP_CHAIN].autonomousConverter,
+  MTN_EXPLORER_URL: REACT_APP_MET_EXPLORER_URL,
+  SENTRY_DSN: REACT_APP_SENTRY_DSN,
   ETH_DEFAULT_GAS_LIMIT: '21000',
-  MET_DEFAULT_GAS_LIMIT: '2000000',
+  MET_DEFAULT_GAS_LIMIT: '200000',
   DEFAULT_GAS_PRICE: '1000000000',
   REQUIRED_PASSWORD_ENTROPY: 72
 }
