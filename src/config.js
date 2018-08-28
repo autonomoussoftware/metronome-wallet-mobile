c86a1557d451bdf5000f853a21401708fe2da7b7
Gabriel Montes <gabriel@bloq.com>
2018-08-28 18:35:59 -0300

    Redo config management

--- src/config.js
+++ src/config.js
@@ -1,21 +1,44 @@
 import Contracts from 'metronome-contracts'
-import Config from '../config'
 
-export default {
-  MTN_TOKEN_ADDR: Contracts.addresses[Config.REACT_APP_ETH_CHAIN].metToken,
-  CONVERTER_ADDR: Contracts.addresses[Config.REACT_APP_ETH_CHAIN].autonomousConverter,
-  MTN_EXPLORER_URL: Config.REACT_APP_MET_EXPLORER_URL,
-  SENTRY_DSN: Config.REACT_APP_SENTRY_DSN,
+import localEnv from '../.config.json'
+
+const devEnv = {
+  ETH_CHAIN: 'ropsten',
+  ETH_WS_API_URL: 'ws://localhost:8545',
+  EXPLORER_INDEXER_URL: 'http://localhost:3005',
+  MET_EXPLORER_URL: 'http://localhost:3004',
+  SENTRY_DSN: ''
+}
+
+const prodEnv = {
+  ETH_CHAIN: 'mainnet',
+  ETH_WS_API_URL: 'wss://eth.wallet.metronome.io:8546',
+  EXPLORER_INDEXER_URL: 'https://indexer.metronome.io',
+  MET_EXPLORER_URL: 'https://explorer.metronome.io',
+  SENTRY_DSN: ''
+}
+
+const createConfig = env => ({
+  MTN_TOKEN_ADDR: Contracts.addresses[env.ETH_CHAIN].metToken,
+  CONVERTER_ADDR: Contracts.addresses[env.ETH_CHAIN].autonomousConverter,
+  MTN_EXPLORER_URL: env.MET_EXPLORER_URL,
+  SENTRY_DSN: env.SENTRY_DSN,
   ETH_DEFAULT_GAS_LIMIT: '21000',
   MET_DEFAULT_GAS_LIMIT: '200000',
   DEFAULT_GAS_PRICE: '1000000000',
   REQUIRED_PASSWORD_ENTROPY: 20,
-  rates: {
-    updateMs: 15000
-  },
   eth: {
-    chain: Config.REACT_APP_ETH_CHAIN,
-    wsApiUrl: Config.REACT_APP_WS_API_URL
+    chain: env.ETH_CHAIN,
+    wsApiUrl: env.ETH_WS_API_URL
+  },
+  explorer: {
+    indexerUrl: env.EXPLORER_INDEXER_URL
   },
   debug: true
-}
+})
+
+const config = __DEV__
+  ? createConfig({ ...prodEnv, ...devEnv, ...localEnv })
+  : createConfig({ ...prodEnv, ...localEnv })
+
+export default config
