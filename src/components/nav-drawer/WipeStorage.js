89064d39506b21ee94e0de88f3249b8983086827
Pablo Enrici <pablo@bloq.com>
2018-10-03 17:42:04 -0300

    Refactor NavigationDrawer into separate components

---
+++ src/components/nav-drawer/WipeStorage.js
@@ -0,0 +1,38 @@
+import * as Keychain from 'react-native-keychain'
+import RNRestart from 'react-native-restart'
+import PropTypes from 'prop-types'
+import config from '../../config'
+import React from 'react'
+import RN from 'react-native'
+
+export default class WipeStorage extends React.Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired
+  }
+
+  resetStorage = () => {
+    RN.Alert.alert(
+      'WARNING',
+      'This will remove all data. Do you want to continue?',
+      [
+        { text: 'NO', onPress: () => {}, style: 'cancel' },
+        {
+          text: 'YES',
+          onPress: () =>
+            Promise.all([
+              RN.AsyncStorage.clear(),
+              Keychain.resetGenericPassword()
+            ]).then(RNRestart.Restart())
+        }
+      ]
+    )
+  }
+
+  render() {
+    return (
+      <RN.TouchableOpacity onPress={this.resetStorage} disabled={!config.debug}>
+        {this.props.children}
+      </RN.TouchableOpacity>
+    )
+  }
+}
