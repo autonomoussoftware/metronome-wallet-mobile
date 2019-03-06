e9c296e8159b79421c59ac4d36029152e0546496
Pablo Enrici <pablo@bloq.com>
2019-03-06 16:59:02 -0300

    Update loading screen to support multiple chains

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -7,7 +7,7 @@ import { PatternView, ChecklistItem, View, Text } from './common'
 
 class Loading extends React.Component {
   static propTypes = {
-    // isMultiChain: PropTypes.bool.isRequired,
+    isMultiChain: PropTypes.bool.isRequired,
     chainsStatus: PropTypes.objectOf(
       PropTypes.shape({
         hasBlockHeight: PropTypes.bool,
@@ -21,6 +21,8 @@ class Loading extends React.Component {
   }
 
   render() {
+    const isCondensed = Object.keys(this.props.chainsStatus).length > 2
+
     return (
       <PatternView>
         <View flex={1} justify="center" align="center">
@@ -30,23 +32,40 @@ class Loading extends React.Component {
             Gathering Information...
           </Text>
 
-          {Object.keys(this.props.chainsStatus).map(chainName => (
+          {Object.keys(this.props.chainsStatus).map((chainName, i) => (
             <View key={chainName}>
+              {this.props.isMultiChain && (
+                <Text
+                  size="xSmall"
+                  opacity={0.8}
+                  mt={i === 0 ? 0 : 3}
+                  ml={isCondensed ? 3.5 : 5.25}
+                  ls={1}
+                >
+                  {(
+                    this.props.chainsStatus[chainName].displayName || ''
+                  ).toUpperCase()}
+                </Text>
+              )}
               <ChecklistItem
+                isCondensed={isCondensed}
                 isActive={this.props.chainsStatus[chainName].hasBlockHeight}
                 text="Blockchain status"
               />
               <ChecklistItem
+                isCondensed={isCondensed}
                 isActive={this.props.chainsStatus[chainName].hasCoinRate}
                 text={`${
                   this.props.chainsStatus[chainName].symbol
                 } exchange data`}
               />
               <ChecklistItem
+                isCondensed={isCondensed}
                 isActive={this.props.chainsStatus[chainName].hasCoinBalance}
                 text={`${this.props.chainsStatus[chainName].symbol} balance`}
               />
               <ChecklistItem
+                isCondensed={isCondensed}
                 isActive={this.props.chainsStatus[chainName].hasMetBalance}
                 text="MET balance"
               />
