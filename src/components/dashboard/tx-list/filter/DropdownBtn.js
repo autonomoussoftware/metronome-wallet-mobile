5e907f0a0c0f154d9fd8a5a4d7aa2f2b7263c3b0
Pablo Enrici <pablo@bloq.com>
2019-03-06 11:25:26 -0300

    Extract caret icon

--- src/components/dashboard/tx-list/filter/DropdownBtn.js
+++ src/components/dashboard/tx-list/filter/DropdownBtn.js
@@ -1,9 +1,9 @@
-import { Svg, G, Line } from 'react-native-svg'
 import PropTypes from 'prop-types'
 import React from 'react'
 import RN from 'react-native'
 
 import { View, Text } from '../../../common'
+import Caret from '../../../icons/CaretIcon'
 
 export default class DropdownBtn extends React.Component {
   static propTypes = {
@@ -39,20 +39,7 @@ export default class DropdownBtn extends React.Component {
         >
           {this.props.label}
         </Text>
-        <Svg viewBox="0 2 21 20" width="14" height="12">
-          <G
-            strokeLinecap="round"
-            strokeWidth="2"
-            fillRule="evenodd"
-            stroke="white"
-            fill="none"
-          >
-            <G rotation={this.props.isOpen ? '90' : '-90'} origin="10, 13">
-              <Line x1="7" y1="13" x2="15" y2="5" />
-              <Line x1="7" y1="14" x2="15" y2="22" />
-            </G>
-          </G>
-        </Svg>
+        <Caret up={this.props.isOpen} />
       </View>
     )
   }
