4380be3b2fbd58f3a4dd7d7873f4f9696a7418e9
Pablo Enrici <pablo@bloq.com>
2019-03-08 18:43:31 -0300

    Add Retry Import drawer UI

--- src/components/port/ConfirmRetryImport.js
+++ src/components/port/ConfirmRetryImport.js
@@ -1,32 +1,45 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { Confirmation, Text } from '../common'
+import { DisplayValue, Confirmation, Text } from '../common'
 
 class ConfirmRetryImport extends React.Component {
   static propTypes = {
     navigation: PropTypes.shape({
       state: PropTypes.shape({
         params: PropTypes.shape({
-          onSubmit: PropTypes.func.isRequired
+          destinationDisplayName: PropTypes.string.isRequired,
+          originDisplayName: PropTypes.string.isRequired,
+          onSubmit: PropTypes.func.isRequired,
+          value: PropTypes.string.isRequired
         }).isRequired
       }).isRequired
     }).isRequired
   }
 
   render() {
-    const { onSubmit } = this.props.navigation.state.params
+    const {
+      destinationDisplayName,
+      originDisplayName,
+      onSubmit,
+      value
+    } = this.props.navigation.state.params
 
     return (
       <Confirmation onSubmit={onSubmit}>
-        <Text size="medium">Confirmation text</Text>
+        <Text size="medium">
+          You will request an import of{' '}
+          <DisplayValue value={value} post=" MET" color="primary" /> from the{' '}
+          <Text color="primary">{originDisplayName}</Text> blockchain to the{' '}
+          <Text color="primary">{destinationDisplayName}</Text> blockchain.
+        </Text>
       </Confirmation>
     )
   }
 }
 
 ConfirmRetryImport.navigationOptions = {
-  headerTitle: 'Confirm'
+  headerTitle: 'Confirm Retry'
 }
 
 export default ConfirmRetryImport
--- src/components/port/RetryImportDrawer.js
+++ src/components/port/RetryImportDrawer.js
@@ -1,19 +1,85 @@
 import withRetryImportFormState from 'metronome-wallet-ui-logic/src/hocs/withRetryImportFormState'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
+import TimeAgo from 'metronome-wallet-ui-logic/src/components/TimeAgo'
 import React from 'react'
+import RN from 'react-native'
 
-import { BaseBtn, View, Text } from '../common'
+import { DisplayValue, GasEditor, BaseBtn, View, Text } from '../common'
+import ReadOnlyField from './ReadOnlyField'
 
 class RetryImportDrawer extends React.Component {
   static propTypes = {
-    // coinSymbol: PropTypes.string.isRequired
+    originDisplayName: PropTypes.string.isRequired,
+    onInputChange: PropTypes.func.isRequired,
+    useCustomGas: PropTypes.bool.isRequired,
+    navigation: PropTypes.shape({
+      setParams: PropTypes.func.isRequired,
+      navigate: PropTypes.func.isRequired
+    }).isRequired,
+    timestamp: PropTypes.number.isRequired,
+    validate: PropTypes.func.isRequired,
+    gasPrice: PropTypes.string,
+    gasLimit: PropTypes.string,
+    errors: PropTypes.object.isRequired,
+    value: PropTypes.string.isRequired,
+    fee: PropTypes.string.isRequired
+  }
+
+  componentDidMount() {
+    this.props.navigation.setParams({
+      onHeaderRightPress: this.onHeaderRightPress
+    })
+  }
+
+  onHeaderRightPress = () => {
+    const { navigation, validate, ...other } = this.props
+    if (validate()) {
+      RN.Keyboard.dismiss()
+      navigation.navigate('ConfirmRetryImport', other)
+    }
   }
 
   render() {
     return (
-      <View bg="dark" flex={1} justify="space-between">
-        <View withKeyboard withHeader flex={1}>
-          <Text>Retry Import Drawer</Text>
+      <View withKeyboard bg="dark">
+        <View flex={1} px={2} py={4} grow={1}>
+          <Text mb={2}>
+            This Port operation was initiated{' '}
+            <Text weight="bold">
+              <TimeAgo timestamp={this.props.timestamp} />
+            </Text>
+            .
+          </Text>
+          <ReadOnlyField
+            value={this.props.originDisplayName}
+            label="Origin Blockchain"
+          />
+
+          <View mt={1} row>
+            <View grow={1}>
+              <ReadOnlyField
+                value={<DisplayValue value={this.props.value} />}
+                label="Amount (MET)"
+              />
+            </View>
+            <View px={1} />
+            <View grow={1}>
+              <ReadOnlyField
+                value={<DisplayValue value={this.props.fee} />}
+                label="Fee (MET)"
+              />
+            </View>
+          </View>
+
+          <View grow={1} mt={2}>
+            <GasEditor
+              onInputChange={this.props.onInputChange}
+              useCustomGas={this.props.useCustomGas}
+              gasLimit={this.props.gasLimit}
+              gasPrice={this.props.gasPrice}
+              errors={this.props.errors}
+            />
+          </View>
         </View>
       </View>
     )
@@ -22,17 +88,34 @@ class RetryImportDrawer extends React.Component {
 
 const EnhancedComponent = withRetryImportFormState(RetryImportDrawer)
 
-EnhancedComponent.navigationOptions = ({ navigation }) => ({
-  headerTitle: 'Retry Metronome Import',
-  headerBackTitle: null,
-  headerRight: (
-    <BaseBtn
-      textProps={{ weight: 'semibold', size: 'medium' }}
-      onPress={navigation.getParam('onHeaderRightPress', null)}
-      label="Review"
-      mr={1}
-    />
-  )
-})
+export default class RetryImport extends React.Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func.isRequired,
+      navigate: PropTypes.func.isRequired,
+      state: PropTypes.object.isRequired
+    }).isRequired
+  }
 
-export default EnhancedComponent
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: 'Retry Import',
+    headerBackTitle: null,
+    headerRight: (
+      <BaseBtn
+        textProps={{ weight: 'semibold', size: 'medium' }}
+        onPress={navigation.getParam('onHeaderRightPress', null)}
+        label="Review"
+        mr={1}
+      />
+    )
+  })
+
+  render() {
+    return (
+      <EnhancedComponent
+        navigation={this.props.navigation}
+        importData={this.props.navigation.state.params}
+      />
+    )
+  }
+}
