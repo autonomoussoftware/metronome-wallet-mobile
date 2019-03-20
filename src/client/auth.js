66fad785530f89c73564144d4a711fd529f0e449
Pablo Enrici <pablo@bloq.com>
2019-03-19 21:38:02 -0300

    Force onboarding when running a fresh install

--- src/client/auth.js
+++ src/client/auth.js
@@ -1,16 +1,21 @@
 import fastPasswordEntropy from 'fast-password-entropy'
 import * as Keychain from 'react-native-keychain'
+import { Settings } from 'react-native'
 import { sha256 } from './crypto'
 
 export const getStringEntropy = str => fastPasswordEntropy(str)
 
 export const getHashedPIN = () =>
-  Keychain.getInternetCredentials('wallet.pin').then(
-    credentials => credentials.password
+  Keychain.getInternetCredentials('wallet.pin').then(({ password }) =>
+    password && Settings.get('useStoredCredentials') === 'true'
+      ? password
+      : false
   )
 
 export const setPIN = pin =>
-  Keychain.setInternetCredentials('wallet.pin', 'pin', sha256(pin))
+  Keychain.setInternetCredentials('wallet.pin', 'pin', sha256(pin)).then(() =>
+    Settings.set({ useStoredCredentials: 'true' })
+  )
 
 export const validatePIN = pin =>
   getHashedPIN().then(
