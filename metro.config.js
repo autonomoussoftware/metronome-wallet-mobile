39942ad26defba83d048d5fee6947ceec17ad74b
Martin Carrera <martin@bloq.com>
2019-04-09 16:49:29 -0300

    Fix crypto?

--- metro.config.js
+++ metro.config.js
@@ -5,12 +5,17 @@
  * @format
  */
 
+const extraNodeModules = require('node-libs-browser')
+
 module.exports = {
+  resolver: {
+    extraNodeModules
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true
+        inlineRequires: false
       }
     })
   }
