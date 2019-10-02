bf97f61e8ea995c978f96feca8b56fe3db1a4f89
Pablo Enrici <pablo@bloq.com>
2019-10-02 19:24:14 -0300

    Upgrade to RN 0.58.6

--- scripts/nuke-caches.sh
+++ scripts/nuke-caches.sh
@@ -1,10 +1,19 @@
-  #!/bin/bash
-  
-  watchman watch-del-all
-  rm -rf ~/.rncache
-  rm -rf ~/Library/Developer/Xcode/DerivedData
-  rm -rf $TMPDIR/metro-* || true
-  rm -rf $TMPDIR/react-native-* || true
-  rm -rf $TMPDIR/haste-* || true
-  rm -rf ios/build
-  rm ios/main.jsbundle
+#!/bin/bash
+
+watchman watch-del-all
+rm -rf ~/.rncache
+rm -rf $TMPDIR/metro-* || true
+rm -rf $TMPDIR/react-native-* || true
+rm -rf $TMPDIR/haste-* || true
+
+# iOS SPECIFIC
+rm -rf ~/Library/Developer/Xcode/DerivedData
+rm -rf ios/build
+rm ios/main.jsbundle
+
+# ANDROID SPECIFIC
+rm android/main.jsbundle
+cd android
+./gradlew clean
+./gradlew :app:dependencies
+cd ..
