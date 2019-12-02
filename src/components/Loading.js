b78cf25146614e608c47bacc0f7ba95bfbd04c5d
Pablo Enrici <pablo@bloq.com>
2019-12-02 18:13:55 -0300

    Update Loading screen to show 1 line per chain and optional details

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,80 +1,129 @@
 import withLoadingState from 'metronome-wallet-ui-logic/src/hocs/withLoadingState'
+import useLoadingState from 'metronome-wallet-ui-logic/src/hooks/useLoadingState'
 import PropTypes from 'prop-types'
 import React from 'react'
 import RN from 'react-native'
 
-import { PatternView, ChecklistItem, View, Text } from './common'
+import { PatternView, ChecklistItem, View, Text, BaseBtn } from './common'
 
-class Loading extends React.Component {
-  static propTypes = {
-    isMultiChain: PropTypes.bool.isRequired,
-    chainsStatus: PropTypes.objectOf(
-      PropTypes.shape({
-        hasBlockHeight: PropTypes.bool,
-        hasCoinBalance: PropTypes.bool,
-        hasMetBalance: PropTypes.bool,
-        hasCoinRate: PropTypes.bool,
-        displayName: PropTypes.string.isRequired,
-        symbol: PropTypes.string.isRequired
-      })
-    ).isRequired
-  }
+function Loading(props) {
+  const { handleDetailsClick, isDetailVisible, isBtnVisible } = useLoadingState(
+    props.chainsStatus
+  )
 
-  render() {
-    const isCondensed = Object.keys(this.props.chainsStatus).length > 2
+  const handlePress = React.useCallback(() => {
+    RN.LayoutAnimation.configureNext({
+      duration: 200,
+      create: {
+        type: RN.LayoutAnimation.Types.easeIn,
+        property: RN.LayoutAnimation.Properties.opacity
+      },
+      update: {
+        type: RN.LayoutAnimation.Types.easeIn,
+        property: RN.LayoutAnimation.Properties.opacity
+      }
+    })
+    handleDetailsClick()
+  }, [])
 
-    return (
-      <PatternView>
-        <View flex={1} justify="center" align="center">
-          <RN.ActivityIndicator size="large" />
+  return (
+    <PatternView>
+      <View flex={1} justify="center" align="center">
+        <RN.ActivityIndicator size="large" />
 
-          <Text size="large" weight="bold" my={4}>
-            Gathering Information...
-          </Text>
+        <Text size="large" weight="bold" my={4}>
+          Gathering Information...
+        </Text>
 
-          {Object.keys(this.props.chainsStatus).map((chainName, i) => (
-            <View key={chainName}>
-              {this.props.isMultiChain && (
-                <Text
-                  size="xSmall"
-                  opacity={0.8}
-                  mt={i === 0 ? 0 : 3}
-                  ml={isCondensed ? 3.5 : 5.25}
-                  ls={1}
+        <View>
+          {Object.keys(props.chainsStatus).map(
+            // eslint-disable-next-line complexity
+            chainName => {
+              const {
+                hasBlockHeight,
+                hasCoinBalance,
+                hasMetBalance,
+                hasCoinRate,
+                displayName,
+                symbol
+              } = props.chainsStatus[chainName]
+
+              const isActive =
+                hasBlockHeight && hasCoinBalance && hasMetBalance && hasCoinRate
+
+              return (
+                <ChecklistItem
+                  isActive={isActive}
+                  text={`${displayName} network`}
+                  key={chainName}
                 >
-                  {(
-                    this.props.chainsStatus[chainName].displayName || ''
-                  ).toUpperCase()}
-                </Text>
-              )}
-              <ChecklistItem
-                isCondensed={isCondensed}
-                isActive={this.props.chainsStatus[chainName].hasBlockHeight}
-                text="Blockchain status"
-              />
-              <ChecklistItem
-                isCondensed={isCondensed}
-                isActive={this.props.chainsStatus[chainName].hasCoinRate}
-                text={`${
-                  this.props.chainsStatus[chainName].symbol
-                } exchange data`}
-              />
-              <ChecklistItem
-                isCondensed={isCondensed}
-                isActive={this.props.chainsStatus[chainName].hasCoinBalance}
-                text={`${this.props.chainsStatus[chainName].symbol} balance`}
-              />
-              <ChecklistItem
-                isCondensed={isCondensed}
-                isActive={this.props.chainsStatus[chainName].hasMetBalance}
-                text="MET balance"
-              />
-            </View>
-          ))}
+                  {isDetailVisible && !isActive && (
+                    <View ml={4.5} opacity={0.5}>
+                      <Text isActive={hasBlockHeight}>
+                        Blockchain status{' '}
+                        {hasBlockHeight && (
+                          <Text color="success" weight="semibold" size="small">
+                            OK
+                          </Text>
+                        )}
+                      </Text>
+                      <Text isActive={hasCoinRate}>
+                        {symbol} exchange data{' '}
+                        {hasCoinRate && (
+                          <Text color="success" weight="semibold" size="small">
+                            OK
+                          </Text>
+                        )}
+                      </Text>
+                      <Text isActive={hasCoinBalance}>
+                        {symbol} balance{' '}
+                        {hasCoinBalance && (
+                          <Text color="success" weight="semibold" size="small">
+                            OK
+                          </Text>
+                        )}
+                      </Text>
+                      <Text isActive={hasMetBalance}>
+                        MET balance{' '}
+                        {hasMetBalance && (
+                          <Text color="success" weight="semibold" size="small">
+                            OK
+                          </Text>
+                        )}
+                      </Text>
+                    </View>
+                  )}
+                </ChecklistItem>
+              )
+            }
+          )}
         </View>
-      </PatternView>
-    )
-  }
+        {isBtnVisible && (
+          <BaseBtn
+            textProps={{ opacity: 0.5, weight: 'semibold', ls: 1 }}
+            onPress={handlePress}
+            label="SHOW DETAILS"
+            size="xSmall"
+            mt={2}
+            mb={-3.8}
+          />
+        )}
+      </View>
+    </PatternView>
+  )
+}
+
+Loading.propTypes = {
+  chainsStatus: PropTypes.objectOf(
+    PropTypes.shape({
+      hasBlockHeight: PropTypes.bool,
+      hasCoinBalance: PropTypes.bool,
+      hasMetBalance: PropTypes.bool,
+      hasCoinRate: PropTypes.bool,
+      displayName: PropTypes.string.isRequired,
+      symbol: PropTypes.string.isRequired
+    })
+  ).isRequired
 }
 
 export default withLoadingState(Loading)
--- src/components/common/ChecklistItem.js
+++ src/components/common/ChecklistItem.js
@@ -7,36 +7,35 @@ import View from './View'
 
 export default class ChecklistItem extends React.Component {
   static propTypes = {
-    isCondensed: PropTypes.bool.isRequired,
     isActive: PropTypes.bool.isRequired,
+    children: PropTypes.node,
     text: PropTypes.string.isRequired
   }
 
   render() {
     return (
-      <View row align="center" p={this.props.isCondensed ? 0.5 : 0.75}>
-        <Svg
-          viewBox="0 0 24 24"
-          height={this.props.isCondensed ? '16' : '24'}
-          width={this.props.isCondensed ? '16' : '24'}
-        >
-          <Path
-            fill={this.props.isActive ? '#45d48d' : '#7d7f89'}
-            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12
+      <View p={0.75}>
+        <View row align="center">
+          <Svg viewBox="0 0 24 24" height="24" width="24">
+            <Path
+              fill={this.props.isActive ? '#45d48d' : '#7d7f89'}
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12
                2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.88
                -11.71L10 14.17l-1.88-1.88a.996.996 0 1 0-1.41 1.41l2.59 2.59c.39
                .39 1.02.39 1.41 0L17.3 9.7a.996.996 0 0 0 0-1.41c-.39-.39-1.03
                -.39-1.42 0z"
-          />
-        </Svg>
-        <Text
-          opacity={this.props.isActive ? 1 : 0.7}
-          weight="semibold"
-          size={this.props.isCondensed ? 'small' : 'medium'}
-          ml={this.props.isCondensed ? 1 : 1.5}
-        >
-          {this.props.text}
-        </Text>
+            />
+          </Svg>
+          <Text
+            opacity={this.props.isActive ? 1 : 0.7}
+            weight="semibold"
+            size="medium"
+            ml={1.5}
+          >
+            {this.props.text}
+          </Text>
+        </View>
+        {this.props.children}
       </View>
     )
   }
